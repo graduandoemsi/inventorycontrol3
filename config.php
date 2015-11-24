cb1eadc335c91e922eb2b7e6960849512a5b9401
Isnack <isnack_yagami@hotmail.com>
2015-11-24 15:04:05 -0200

    database

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define("HOST", 'control.csbt5nxjd4lq.us-east-1.rds.amazonaws.com');
 define("DB_NAME", 'control');
 define("DB_USER", 'root');
-define("DB_PASS", 'isroru2015');
+define("DB_PASS", '');
 //rob
 
 ?>
